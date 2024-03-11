5dfb7ff6d5fd5539b373456757215dac13cfba09
aperez26323 <aperez26323@alumnos.uemc.es>
2024-03-11 18:39:14 +0100

    rarete

---
+++ descifrador.py
@@ -0,0 +1,105 @@
+FILE_PATH = "cifrado.txt"
+LETRAS_ORDENADAS_FRECUENCIA = [
+    "E",
+    "A",
+    "O",
+    "S",
+    "R",
+    "N",
+    "I",
+    "D",
+    "L",
+    "C",
+    "T",
+    "U",
+    "M",
+    "P",
+    "B",
+    "G",
+    "V",
+    "Y",
+    "Q",
+    "H",
+    "F",
+    "Z",
+    "J",
+    "Ñ",
+    "X",
+    "K",
+    "W",
+]
+
+
+def cuenta_letras(texto) -> [()]:
+    cuenta = {}
+    for letra in texto:
+        if letra in cuenta:
+            cuenta[letra] += 1
+        else:
+            cuenta[letra] = 1
+    return sorted(cuenta.items(), key=lambda x: x[1], reverse=True)
+
+
+def letter_swapper(texto) -> str:
+    letra_sustituible = ""
+    while letra_sustituible.upper() not in LETRAS_ORDENADAS_FRECUENCIA:
+        letra_sustituible = input("Introduce la letra que quieres sustituir: ")
+    
+    letra_sustituta = 0
+    while letra_sustituta not in LETRAS_ORDENADAS_FRECUENCIA:
+        letra_sustituta = input(f"Introduce la letra de sustitución para {letra_sustituible}: ")
+
+    texto_resultado = ""
+    for letra in texto:
+        if letra == letra_sustituible:
+            letra = letra_sustituta
+        texto_resultado += letra
+    
+    return texto_resultado
+
+def cambio_frecuencia(texto, frecuencia):
+    letra_sustituta_dict = dict(LETRAS_ORDENADAS_FRECUENCIA)
+    texto_resultado = ""
+
+    for letra in texto:
+        if letra.upper() in letra_sustituta_dict:
+            letra_sustituta = letra_sustituta_dict[letra.upper()]
+            if letra.islower():
+                letra_sustituta = letra_sustituta.lower()
+            texto_resultado += letra_sustituta
+        else:
+            texto_resultado += letra
+
+    return texto_resultado
+
+def bucle_descifrado(texto, frecuencia) -> str:
+    empezar = True
+    while empezar:
+        texto_auxiliar = texto
+        continuar = True
+        while continuar:
+            print(texto_auxiliar)
+            print(f"La frecuencia original de las letras es:\n{frecuencia}")
+            texto_auxiliar = cambio_frecuencia(texto_auxiliar, frecuencia)
+            continuar = (input("\nDesea continuar? (Y/N) ").upper() == "Y")
+        
+        empezar = (input("\nDesea volver a empezar? (Y/N) ").upper() == "Y")
+        
+    return texto_auxiliar
+
+def main():
+    with open(FILE_PATH, "r") as archivo:
+        texto_cifrado = archivo.read()
+
+    cuenta = cuenta_letras(texto_cifrado)
+    with open("estadistica.txt", "w") as estadistica:
+        frecuencia = "\n".join([f"{tupla[0]}: {tupla[1]} (potencialmente {LETRAS_ORDENADAS_FRECUENCIA[indice]})" for indice, tupla in enumerate(cuenta)])
+        estadistica.write(frecuencia)
+
+    texto_resultado = bucle_descifrado(texto_cifrado, frecuencia)
+
+    with open("resultado.txt", 'w') as resultado:
+        resultado.write(texto_auxiliar)
+
+if __name__ == "__main__":
+    main()
